752fcb372de4de2cfe8322b8c78ed62282903bff
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2024-12-05 19:53:26 +0100

    add user , update with db

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationMA.Models;
+
+namespace WebApplicationMA.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(UserSignup model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                ViewBag.Message = "Signup successful!";
+                return View("SignupSuccess", model);
+            }
+
+           
+            return View(model);
+        }
+    }
+}
---
+++ Models/UserSignup.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMA.Models
+{
+    public class UserSignup
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        public required string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public required string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public required string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public required string Password { get; set; } = string.Empty;
+    }
+}
+
+
